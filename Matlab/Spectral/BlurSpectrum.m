af8258e802a82959a5fd8cf1e4e25af3abb74df5
Jeffrey J. Early <jeffrey@jeffreyearly.com>
2016-08-06 15:06:24 -0700

    Made some changes to the Blurring algorithm? Ugh. Wish I remembered why I did that.

--- Matlab/Spectral/BlurSpectrum.m
+++ Matlab/Spectral/BlurSpectrum.m
@@ -3,10 +3,11 @@ function [ S_blur ] = BlurSpectrum( omega, S )
 %   Detailed explanation goes here
 N=length(omega);
 MF = floor(N/2)+1;
-SpecPermute = [S(MF:N); S(1:MF-1)]; % Permute Spectrum for ifft
+SpecPermute = ifftshift(S); %[S(MF:N); S(1:MF-1)]; % Permute Spectrum for ifft
 Autocov = ifft(SpecPermute); % Autocovariance of Spectrum
+% TriangleKernel = ifftshift((1-([ceil(N/2):-1:1 0:MF-2]')/MF));
 
-TriangleKernel = (1-([0:MF-1 ceil(N/2)-1:-1:1]/N))'; % Triange kernel (works for odd and even N)
+TriangleKernel = (1-([0:MF-1 ceil(N/2)-1:-1:1]/MF))'; % Triange kernel (works for odd and even N)
 S_blur = fftshift(real(fft(TriangleKernel.*Autocov))); % Blurred Spectrum
 end
 
