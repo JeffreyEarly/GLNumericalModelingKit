7f57096ff471640408d3f7fa29804cc3ba74455a
JeffreyEarly <jeffrey@earlyinnovations.com>
2017-03-10 09:34:35 -0800

    Fixing inverse Chebyshev transform to assume symmetric input

--- Matlab/Spectral/ifct.m
+++ Matlab/Spectral/ifct.m
@@ -1,21 +1,10 @@
 function u = ifct(uh)
-% Inverse Fast Chebyshev Transform 
-%
-%    uh : Discrete Chebyshev Transform Coefficients.
-%    u  : Function values evaluted at Chebyshev Gauss Lobatto nodes
-%         with nodes ordered increasingly x_i=[-1,...,1}
-%         for i=1,2...,N
-%
-% By Allan P. Engsig-Karup, apek@imm.dtu.dk.
+%% Inverse Fast Chebyshev Transform 
 
-N = length(uh);
-u=fft([uh(1); [uh(2:N-1); uh(N)*2; uh(N-1:-1:2)]*0.5]); 
-u=(u(1:N)); % reverse ordering due to Matlab's fft.
+N = length(uh) - 1;
+s = N*[uh(1)*2; uh(2:N); uh(end)*2];
+u = ifft([s; flip(s(2:end-1))],'symmetric');
 
-if any(imag(u)./abs(u) > 1e-6)
-	disp('Inverse Fast Chebyshev Transform returned imaginary values. Something went wrong!')
-end
-
-u = real(u);
+u=u(1:N+1);
 
 return
