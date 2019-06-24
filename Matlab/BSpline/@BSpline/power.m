06ab0cfa7003b69cccc568671d0ed8992211776b
JeffreyEarly <jeffrey@jeffreyearly.com>
2019-06-24 14:14:18 -0700

    Update power.m

--- Matlab/BSpline/@BSpline/power.m
+++ Matlab/BSpline/@BSpline/power.m
@@ -7,8 +7,9 @@ else
     ts = BSpline.PointsOfSupport(spline.t_knot,spline.K,0);
     g = spline.ValueAtPoints(ts);
     g(abs(2*eps)>g) = 0;
-%     splineb = InterpolatingSpline(ts,(g.^b),ceil(b*spline.K));
-    K = ceil(b*spline.K);
+    %     splineb = InterpolatingSpline(ts,(g.^b),ceil(b*spline.K));
+%         K = ceil(b*spline.K);
+    K = spline.K;
     t_knot = InterpolatingSpline.KnotPointsForPoints(ts,K);
     if exist('constraints','var') && ~isempty(constraints)
         splineb = ConstrainedSpline(ts,(g.^b),K,t_knot,[],struct('global',ShapeConstraint.positive));
@@ -17,6 +18,6 @@ else
         m = X\(g.^b);
         splineb = BSpline(spline.K,spline.t_knot,m);
     end
-
+    
 end
 
