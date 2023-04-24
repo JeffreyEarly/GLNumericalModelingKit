bb029633290c7dffbd43ca7c9f4917d602bd6f06
JeffreyEarly <jeffrey@jeffreyearly.com>
2023-04-23 20:29:03 -0700

    Update InterpolatingSpline.m

--- Matlab/BSpline/@InterpolatingSpline/InterpolatingSpline.m
+++ Matlab/BSpline/@InterpolatingSpline/InterpolatingSpline.m
@@ -82,6 +82,53 @@ classdef InterpolatingSpline < BSpline
     
     methods (Static)
                 
+        function t_knot = KnotPointsForDataPoints( t, options)
+            arguments
+                t (:,1) double
+                options.K (1,1) double {mustBePositive,mustBeInteger} = 4
+                options.M (1,1) double {mustBePositive,mustBeInteger} = length(t)
+            end
+            N = length(t);
+            if options.M > N
+                options.M = N;
+            end
+            t_pseudo = interp1((0:N-1)',t,linspace(0,N-1,options.M).');
+            K = options.K;
+
+            if length(t_pseudo) > 2
+                if mod(K,2) == 1
+                    % Odd spline order, so knots go in between points.
+                    dt = diff(t_pseudo);
+
+                    % This gives us N+1 knot points
+                    t_knot = [t_pseudo(1); t_pseudo(1:end-1)+dt/2; t_pseudo(end)];
+
+                    % Now remove start and end knots
+                    for i=1:((K-1)/2)
+                        t_knot(2) = [];
+                        t_knot(end-1) = [];
+                    end
+
+                else
+                    t_knot = t_pseudo;
+
+                    % Now remove start and end knots
+                    for i=1:((K-2)/2)
+                        t_knot(2) = [];
+                        t_knot(end-1) = [];
+                    end
+
+                end
+            else
+                t_knot = t_pseudo;
+            end
+            
+            % Now we increase the multiplicity of the knot points at the beginning and
+            % the end of the interval so that the splines do not extend past the end
+            % points.
+            t_knot = [repmat(t_knot(1),K-1,1); t_knot; repmat(t_knot(end),K-1,1)];
+        end
+
         function t_knot = KnotPointsForPoints( t, K, DF )
             %% KnotPointsForPoints
             %
